b666e98f3e6db8456ddb02b156df870d3919e056
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-01-31 17:48:17 -0600

    Time: 396 ms (45.08%), Space: 69.1 MB (15.57%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -1,12 +1,18 @@
-const longestConsecutive = (nums) => {
-  let maxLength = 0;
-  let set = new Set(nums); //Intialize set with nums array
-
-  for (let num of nums) {
-    if (set.has(num - 1)) continue; // Not the start of a new consecutive list, so skip.
-    let length = 1;
-    while (set.has(num + length)) length++; // Build up the length as we find the next consecutive num
-    maxLength = Math.max(length, maxLength); // Update max
-  }
-  return maxLength;
+const longestConsecutive = function(nums) {
+    let len = nums.length
+    if(len<2) return len;
+    
+    let map = {}
+    for(let i=0;i<nums.length;i++){
+        map[nums[i]] = 1
+    }
+    let res = 0;
+    for(let i=0;i<len;i++){
+        if(!map[nums[i]-1]){
+            let y = nums[i] + 1;
+            while(map[y]) y++
+            res = Math.max(res, y-nums[i]);
+        }
+    }
+    return res
 };
