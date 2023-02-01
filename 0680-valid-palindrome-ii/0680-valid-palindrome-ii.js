c0cc3c05d63c9b17b46721cac83515db1c45f00d
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-01-31 18:59:14 -0600

    Time: 94 ms (58.97%), Space: 51 MB (31.01%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -1,20 +1,11 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
 var validPalindrome = function(s) {
-    return findPalindrome(s, 0, s.length-1, false);
+    return checkIfPalindrome(s, 0, s.length-1, false);
 };
 
-
-const findPalindrome = (s, start, end, flag) => {
+const checkIfPalindrome = (s, start, end, flag) => {
     if(start>=end) return true;
-    
     if(flag && s[start] != s[end]) return false;
-    
     if(s[start] != s[end])
-        return (findPalindrome(s, start+1, end, true) || findPalindrome(s, start, end-1, true))
-    
-    return findPalindrome(s, start+1, end-1, flag)
-    
+        return (checkIfPalindrome(s, start+1, end, true) || checkIfPalindrome(s, start, end-1, true));
+    return checkIfPalindrome(s, start+1, end-1, flag);    
 }
