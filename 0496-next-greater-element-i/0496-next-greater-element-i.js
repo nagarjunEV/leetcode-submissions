6312e4071e2cb2080217c8afb1ab6ecfdfb356d6
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-02 12:16:32 -0600

    Time: 83 ms (43.00%), Space: 44.4 MB (29.52%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -1,18 +1,24 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
 var nextGreaterElement = function(nums1, nums2) {
-    let res = nums1.map(x=>-1)
+    let res = nums1.map(x => -1);
     let map = {}
+    
     for(let i=0;i<nums1.length;i++)
         map[nums1[i]] = i
     
     let stack = []
-    
     for(let i=0;i<nums2.length;i++){
-        while(stack.length>0 && nums2[i] > stack[stack.length-1]){
-            let temp = stack.pop()
-            res[map[temp]] = nums2[i]                
+        let curr = nums2[i] 
+        while(stack.length>0 && curr > stack[stack.length-1]){
+            let temp = stack.pop();
+            res[map[temp]] = curr
         }
-        if(map[nums2[i]] !== undefined)
-            stack.push(nums2[i])
+        if(map[curr]!=undefined)
+            stack.push(curr)
     }
     return res
 };
