cefe7b69a4507829f1b5a5930a82191cf1a98d56
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-02 12:01:00 -0600

    Time: 74 ms (67.45%), Space: 44.5 MB (24.38%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,17 +4,26 @@ var nextGreaterElement = function(nums1, nums2) {
     for(let i=0;i<nums1.length;i++)
         map[nums1[i]] = i
     
+    let stack = []
+    
     for(let i=0;i<nums2.length;i++){
-        if(map[nums2[i]]!== undefined){
-            // console.log(map[nums2[i]])
-            
-            for(let j=i+1; j<nums2.length; j++){
-                if(nums2[j]>nums2[i]){
-                    res[map[nums2[i]]] = nums2[j]
-                    break
-                }
-            }
+        while(stack.length>0 && nums2[i] > stack[stack.length-1]){
+            let temp = stack.pop()
+            res[map[temp]] = nums2[i]                
         }
+        if(map[nums2[i]] !== undefined)
+            stack.push(nums2[i])
     }
+    
+    // for(let i=0;i<nums2.length;i++){
+    //     if(map[nums2[i]] !== undefined){
+    //         for(let j=i+1; j<nums2.length; j++){
+    //             if(nums2[j]>nums2[i]){
+    //                 res[map[nums2[i]]] = nums2[j]
+    //                 break
+    //             }
+    //         }
+    //     }
+    // }
     return res
 };
