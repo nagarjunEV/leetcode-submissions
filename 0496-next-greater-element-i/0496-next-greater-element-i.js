a55b06d28d60aad3501ba692b5151f9d0f2fa294
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-02 12:01:21 -0600

    Time: 79 ms (50.80%), Space: 43.9 MB (59.73%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -14,16 +14,5 @@ var nextGreaterElement = function(nums1, nums2) {
         if(map[nums2[i]] !== undefined)
             stack.push(nums2[i])
     }
-    
-    // for(let i=0;i<nums2.length;i++){
-    //     if(map[nums2[i]] !== undefined){
-    //         for(let j=i+1; j<nums2.length; j++){
-    //             if(nums2[j]>nums2[i]){
-    //                 res[map[nums2[i]]] = nums2[j]
-    //                 break
-    //             }
-    //         }
-    //     }
-    // }
     return res
 };
