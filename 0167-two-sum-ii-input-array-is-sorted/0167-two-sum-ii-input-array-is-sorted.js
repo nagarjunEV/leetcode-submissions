7a8a2ffac202d3a25e34f3190aaaec1963b8e831
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-02 19:47:35 -0600

    Time: 67 ms (78.19%), Space: 43.2 MB (30.84%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -6,13 +6,12 @@
 var twoSum = function(nums, target) {
     let left = 0, right = nums.length - 1;
     
-    while(left<right){
-        if(nums[left]+nums[right] == target)
-            break;
+    while(nums[left]+nums[right] != target){
         if(nums[left]+nums[right]>target)
             right--
         else
             left++
     }
+        
     return [left+1, right+1]
 };
