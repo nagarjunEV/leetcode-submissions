f1b8256490d99050d8917a1e706865f5def27626
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-02 18:21:48 -0600

    Time: 168 ms (80.12%), Space: 71.9 MB (54.31%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    if(head.next == null) return true
+    
+    let stack = []
+    let temp = head;
+    
+    while(temp){
+        stack.push(temp.val);
+        temp = temp.next;
+    }
+    
+    while(head){
+        if(head.val != stack.pop())
+            return false
+        
+        head = head.next;
+    }
+    return true
+};
