08e8664543d0ded85270198cb025557ab9662cba
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-06 18:47:42 -0600

    Time: 74 ms (90.31%), Space: 45 MB (35.66%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
@@ -3,11 +3,5 @@
  * @return {number[]}
  */
 var separateDigits = function(nums) {
-    let str = "", res = [];
-    while(nums.length){
-        str = nums.pop() + str;
-    }
-    for(let i=0;i<str.length;i++)
-        res.push(str[i])
-    return res
+    return nums.join('').split('');
 };
