2a4d2aefa97560d0bd7e5e5c236878f228186b66
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-07 10:19:15 -0600

    Time: 101 ms (55.52%), Space: 46.6 MB (62.00%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,16 +3,15 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    if(nums.length == 1) return nums
+    if(nums.length == 1) return nums;
     
     let zero = 0;
     for(let i=0;i<nums.length;i++){
         if(nums[i]!=0){
-            let temp = nums[zero]
+            let temp = nums[zero];
             nums[zero] = nums[i]
-            nums[i] = temp
+            nums[i] = temp;
             zero++
         }
-        
     }
 };
