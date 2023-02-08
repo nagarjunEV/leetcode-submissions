5af9464d7ab8c295261e3266ec6a23e025301fe3
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-07 18:41:30 -0600

    Time: 65 ms (66.95%), Space: 42 MB (61.50%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -7,9 +7,7 @@ var removeElement = function(nums, val) {
     let target = 0;
     for(let i=0;i<nums.length;i++){
         if(nums[i]!=val){
-            let temp = nums[target];
             nums[target] = nums[i];
-            nums[i] = temp;
             target++;
         }
     }
