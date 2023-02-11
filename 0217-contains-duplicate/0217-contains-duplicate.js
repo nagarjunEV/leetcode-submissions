f1a82796afef20b6f0f9f94f388954838f8db333
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-11 15:52:44 -0600

    Time: 92 ms (76.86%), Space: 54.4 MB (25.92%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    if(nums.length<2) return false
+    let map = {}
+    
+    for(let i=0;i<nums.length;i++){
+        if(map[nums[i]] === 1)
+            return true
+        map[nums[i]] = 1
+    }
+    return false
+};
