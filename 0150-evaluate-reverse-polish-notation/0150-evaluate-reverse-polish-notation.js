09291633895c3e5887e0ab216b586ac0ffe7f471
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-12 17:57:16 -0600

    Time: 73 ms (82.00%), Space: 44.4 MB (79.48%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -3,8 +3,13 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-    if(tokens.length === 1) return tokens[0];
-    let map = {'+':1, '-':1, '*':1, '/':1}
+    let map = {
+        '+': (a,b) => a+b,
+        '-': (a,b) => a-b,
+        '*': (a,b) => a*b,
+        '/': (a,b) => a/b >= 0 ? Math.floor(a/b) : Math.ceil(a/b)
+    }
+    
     let stack = []
     for(let i=0; i<tokens.length; i++){
         if(map[tokens[i]] == undefined)
@@ -13,15 +18,7 @@ var evalRPN = function(tokens) {
             let a = Number(stack.pop());
             let b = Number(stack.pop())
             let op = tokens[i]
-            if(op == '/'){
-                (Math.floor(b/a)) >= 0 ? stack.push(Math.floor(b/a)) :  stack.push(Math.ceil(b/a))
-            }
-            else if(op == '*')
-                stack.push(b * a)
-            else if(op == '-')
-                stack.push(b - a)
-            else if(op == '+')
-                stack.push(b + a)
+            stack.push(map[op](b, a))
         }
     }
     return stack.pop()
