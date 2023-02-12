b03f334c2980c81d590da54b76d4369f4d4a0fc7
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-12 14:33:57 -0600

    Time: 74 ms (45.04%), Space: 42.8 MB (84.68%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -1,16 +1,14 @@
-/**
- * @param {number[]} numbers
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
-    let i = 0, j = nums.length - 1;
-    while(i<j){
-        if(nums[i]+nums[j] == target)
-            return [i+1, j+1];
-        else if(nums[i]+nums[j] > target)
-            j--;
-        else
-            i++;
+const twoSum = (numbers, target) => {
+    let p1 = 0
+    let p2 = numbers.length - 1
+    
+    while (numbers[p1] + numbers[p2] !== target) {
+        if (numbers[p1] + numbers[p2] > target) {
+            p2--
+        } else {
+            p1++
+        }
     }
-};
+    
+    return [p1 + 1, p2 + 1]
+}
