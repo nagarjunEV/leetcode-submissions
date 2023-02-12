c6c08fdb6136db67f467f3ecfa51da5d3cf0eccd
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-12 14:26:17 -0600

    Time: 71 ms (87.47%), Space: 44.5 MB (68.71%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,10 +3,10 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s = s.replace(/[^a-z0-9]*/gi, "").toLowerCase();
-    let left = 0, right = s.length - 1;
-    while(left<=right){
-        if(s[left++] != s[right--])
+    s = s.replace(/[^0-9a-zA-Z]*/g, "").toLowerCase();
+    let i = 0, j = s.length-1;
+    while(i<=j){
+        if(s[i++]!=s[j--])
             return false
     }
     return true
