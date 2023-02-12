23bf38d57241b7b039f1c27f0b0f79db8e65d419
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-11 18:30:05 -0600

    Time: 76 ms (91.23%), Space: 43 MB (82.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -1,15 +1,14 @@
 var isAnagram = function(s, t) {
     if(s.length != t.length) return false;
-    let map = {}
-    for(let i=0;i<s.length;i++)
-        map[s[i]] = map[s[i]] ? ++map[s[i]] : 1
-    
-    for(let i=0;i<t.length;i++){
-        if(!map[t[i]])
-            return false
-        else{
-            map[t[i]] = --map[t[i]]            
-        }
+    let freqArr = new Array(26).fill(0);
+    for(let i=0;i<s.length;i++){
+        let index = s[i].charCodeAt(0) - 'a'.charCodeAt(0);
+        freqArr[index] += 1;
     }
+    for(let i=0;i<t.length;i++){
+        let index = t[i].charCodeAt(0) - 'a'.charCodeAt(0);
+        freqArr[index] -= 1;
+    }
+    if(freqArr.some(x => x!=0)) return false
     return true
 };
