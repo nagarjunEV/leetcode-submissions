ff7bdacb57b30ff49935de297d15284e8179e580
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-16 01:05:33 -0600

    Time: 62 ms (76.28%), Space: 42 MB (57.74%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,15 +3,14 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    for(let i=digits.length-1; i>=0; i--){
-        if(digits[i] != 9){
-            digits[i]++;
-            return digits;
-        }
-        digits[i] = 0;
-        if(i==0) {
-            digits.unshift(1);
-            return digits;
-        }
-    }
+   for(let i=digits.length-1;i>=0;i--){
+       digits[i]++;
+       if(digits[i]>9){
+           digits[i] = 0;
+       }else{
+           return digits;
+       }
+   }
+    digits.unshift(1);
+    return digits;
 };
