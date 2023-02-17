36d21696af39296eaa8109dfb76ee875d7716706
nagarjunEV <90735972+nagarjunEV@users.noreply.github.com>
2023-02-16 20:20:39 -0600

    Time: 505 ms (39.16%), Space: 69.6 MB (13.33%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -1,16 +1,19 @@
-const longestConsecutive = function(nums) {
-    let len = nums.length
-    if(len<2) return len;   
-    let map = {}
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    let res = 0, map = {};
+    nums.forEach(x => map[x] = 1);
+    
     for(let i=0;i<nums.length;i++){
-        map[nums[i]] = 1
-    }
-    let res = 0;
-    for(let i=0;i<len;i++){
-        if(!map[nums[i]-1]){
-            let y = nums[i] + 1;
-            while(map[y]) y++
-            res = Math.max(res, y-nums[i]);
+        let num = nums[i];
+        if(map[num-1] == undefined){
+            let j = num+1;
+            while(map[j] == 1){
+                j++;
+            }
+            res = Math.max(res, j-num);
         }
     }
     return res
