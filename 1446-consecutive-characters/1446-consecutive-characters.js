df5d5c9dc7d9b01cb58effed4eb8a7d228457be3
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2023-02-19 13:51:35 -0600

    Time: 73 ms (61.02%), Space: 41.9 MB (97.18%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.js
+++ 1446-consecutive-characters/1446-consecutive-characters.js
@@ -3,9 +3,9 @@
  * @return {number}
  */
 var maxPower = function(s) {
-    if(s.length==1) return 1
-    let res = 0, count = 1;
+    if(s.length==1) return 1;
     
+    let res = 0, count = 1;
     for(let i=1;i<s.length;i++){
         count = 1;
         while(i<s.length && s.charCodeAt(i-1) == s.charCodeAt(i)){
