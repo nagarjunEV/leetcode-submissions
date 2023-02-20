93f40d8fa87977915238b9106b9119b0c4418d2c
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2023-02-19 18:08:43 -0600

    Time: 82 ms (32.00%), Space: 44.1 MB (51.88%) - LeetHub

--- 0415-add-strings/0415-add-strings.js
+++ 0415-add-strings/0415-add-strings.js
@@ -4,14 +4,13 @@
  * @return {string}
  */
 var addStrings = function(num1, num2) {
-    let m = num1.length-1, n = num2.length-1, res = [], max = Math.max(m, n);
+    let m = num1.length-1, n = num2.length-1, res = "", max = Math.max(m, n);
     let carry = 0
     while(m>=0 || n>=0){
         let sum = ((Number(num1[m--]) || 0) + Number((num2[n--]) || 0)) + carry;
         carry = (sum>9) ? 1 : 0;
-        res[max--] = sum%10
+        res = (sum%10) + res
     }
-    res = res.join('')
     if(carry) res = '1' + res; 
     return res
 };
