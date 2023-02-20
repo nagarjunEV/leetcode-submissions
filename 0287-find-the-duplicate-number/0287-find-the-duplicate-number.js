800ce0a1b52e474fcd34bde743736af465d96c4c
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2023-02-19 19:18:28 -0600

    Time: 81 ms (92.56%), Space: 49.6 MB (85.18%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    
+    for(let i=0;i<nums.length;i++){
+        let index = Math.abs(nums[i]);
+        if(nums[index]<0)
+            return index
+        nums[index] = -nums[index]
+    }
+};
