d40451287245fd5a1355d96a7bde36478a734f33
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2023-02-20 18:36:35 -0600

    Time: 96 ms (41.67%), Space: 43.4 MB (70.45%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.js
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -1,13 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
 var threeSumClosest = function(nums, target) {
-    let len = nums.length;
-    if(len == 3) return nums.reduce((a, b) => a+b);
     let res = Number.MAX_VALUE;
     nums = nums.sort((a, b) => a-b);
-    for(let i=0;i<len-2;i++){
-        let j = i+1, k = len-1;
+    for(let i=0;i<nums.length-2;i++){
+        // if(nums[i]>target)
+            // break;
+        let j = i+1, k = nums.length - 1;
         while(j<k){
-            let sum = nums[i] + nums[j] + nums[k];
-            if(Math.abs(sum-target)<Math.abs(res-target)) res = sum;
+            const sum = nums[i] + nums[j] + nums[k];
+            if(Math.abs(sum-target) < Math.abs(res-target))
+                res = sum
             if(sum>target)
                 k--;
             else
