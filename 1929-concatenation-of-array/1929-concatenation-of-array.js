026a345ac06b4a85655dc24add6e711c896f0af0
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2023-03-04 19:55:33 -0600

    Time: 69 ms (96.03%), Space: 45.1 MB (59.32%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,10 +3,9 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let ans = [], len = nums.length;
+    let len = nums.length;
     for(let i=0;i<len;i++){
-        ans[i] = nums[i]
-        ans[i+len] = nums[i]
+        nums[i+len] = nums[i]
     }
-    return ans
+    return nums
 };
