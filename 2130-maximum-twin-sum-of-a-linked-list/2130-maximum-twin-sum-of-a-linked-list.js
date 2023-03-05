1354432163b015736146533fcade58f648ee835d
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2023-03-04 20:08:00 -0600

    Time: 160 ms (92.98%), Space: 65.7 MB (74.79%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.js
@@ -0,0 +1,28 @@
+const reverseLinkedList = head => {
+    let prev = null;
+    while(head){
+        let temp = head.next;
+        head.next = prev;
+        prev = head;
+        head = temp
+    }
+    return prev;
+}
+
+var pairSum = function(head) {
+    if(head.next.next == null) return head.val + head.next.val;
+
+    let max = 0, fast = head, slow = head;
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    slow = reverseLinkedList(slow);
+    
+    while(head && slow){
+        max = Math.max(head.val + slow.val, max);
+        head = head.next;
+        slow = slow.next
+    }
+    return max
+};
