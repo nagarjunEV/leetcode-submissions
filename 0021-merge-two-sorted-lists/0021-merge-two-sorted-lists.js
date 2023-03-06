64995e3e9a5077090e53212e2b6fc27cb350ddb9
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2023-03-05 22:21:49 -0600

    Time: 61 ms (94.78%), Space: 44.2 MB (55.88%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,20 @@
+var mergeTwoLists = function(list1, list2) {
+    let curr = new ListNode(0);
+    let res = curr;
+    while(list1 && list2){
+        if(list1.val <= list2.val){
+            curr.next = list1;
+            list1 = list1.next            
+        }else{
+            curr.next = list2;
+            list2 = list2.next            
+        }
+        curr = curr.next
+    }
+    if(list1)
+        curr.next = list1
+    if(list2)
+        curr.next = list2
+    
+    return res.next
+};
