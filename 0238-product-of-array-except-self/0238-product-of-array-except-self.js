a8e999e821aae4248695b3b24da86f4aeb59175e
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2023-03-12 18:19:53 -0500

    Time: 119 ms (36.67%), Space: 54.2 MB (61.33%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -1,28 +1,20 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
 var productExceptSelf = function(nums) {
-    let prefix = [nums[0]]
-    let postfix = []
-    let res = []
-    for(let i=1;i<nums.length;i++){
-        prefix[i] = prefix[i-1] * nums[i]
-    }
-    for(let i=nums.length-1;i>=0;i--){
-        if(i == nums.length-1)
-            postfix[i] = nums[i]
-        else
-            postfix[i] = nums[i] * postfix[i+1]
+    let prefix = [], postfix = [], res = [];
+  
+    for(let i=0;i<nums.length;i++){
+      if(i==0) prefix[i] = nums[i]
+      else prefix[i] = nums[i] * prefix[i-1]
     }
     
+    for(let i=nums.length-1;i>0;i--){
+      if(i==nums.length-1) postfix[i] = nums[i]
+      else postfix[i] = nums[i] * postfix[i+1]
+    }
+  
     for(let i=0;i<nums.length;i++){
-        if(i==0)
-            res[i] = postfix[i+1]
-        else if(i==nums.length-1)
-            res[i] = prefix[i-1]
-        else
-            res[i] = prefix[i-1] * postfix[i+1]
+        if(i==0) res[i] = postfix[i+1]
+        else if(i==nums.length-1) res[i] = prefix[i-1]
+        else res[i] = prefix[i-1] * postfix[i+1]
     }
     return res
 };
