095dc5ff5aa1433acc9d698db2722a671072612e
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2023-03-12 18:43:03 -0500

    Time: 64 ms (90.04%), Space: 45.1 MB (19.62%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -6,7 +6,7 @@ var findMaxConsecutiveOnes = function(nums) {
     let res = 0;
     for(let i=0;i<nums.length;i++){
         let count = 0;
-        while(nums[i] == 1) count++, i++;
+        while(i<nums.length && nums[i] == 1) count++, i++;
         res = Math.max(res, count)
     }
     return res
