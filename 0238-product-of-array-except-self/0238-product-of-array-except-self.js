df1afbefe6ce840a44236228c62610b4a65227c4
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2023-03-12 18:23:15 -0500

    Time: 126 ms (27.38%), Space: 56 MB (25.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -1,20 +1,17 @@
 var productExceptSelf = function(nums) {
-    let prefix = [], postfix = [], res = [];
-  
+    let res = [];
+    let left = 1, right = 1;
+    
     for(let i=0;i<nums.length;i++){
-      if(i==0) prefix[i] = nums[i]
-      else prefix[i] = nums[i] * prefix[i-1]
+        res[i] = left;
+        left *= nums[i]
     }
     
-    for(let i=nums.length-1;i>0;i--){
-      if(i==nums.length-1) postfix[i] = nums[i]
-      else postfix[i] = nums[i] * postfix[i+1]
+    for(let i=nums.length-1;i>=0;i--){
+        res[i] *= right;
+        right *= nums[i]
     }
-  
-    for(let i=0;i<nums.length;i++){
-        if(i==0) res[i] = postfix[i+1]
-        else if(i==nums.length-1) res[i] = prefix[i-1]
-        else res[i] = prefix[i-1] * postfix[i+1]
-    }
+    
     return res
+
 };
