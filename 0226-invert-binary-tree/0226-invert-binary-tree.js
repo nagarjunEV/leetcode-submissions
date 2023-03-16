399e5c3575d98340ad2fdea71ce481c9aee40981
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2023-03-16 11:58:59 -0500

    Time: 49 ms (98.44%), Space: 42.7 MB (8.37%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -13,10 +13,8 @@
 var invertTree = function(root) {
     if(!root) return null;
     
-    let left = invertTree(root.left)
-    let right = invertTree(root.right)
-    
-    root.left = right;
-    root.right = left;
+    [root.left, root.right] = [root.right, root.left]
+    invertTree(root.left);
+    invertTree(root.right);
     return root
 };
