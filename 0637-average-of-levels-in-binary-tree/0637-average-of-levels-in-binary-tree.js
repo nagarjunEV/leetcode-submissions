b68b5d4921762204805098fad81978d90d56e228
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2023-04-02 21:33:56 -0500

    Time: 87 ms (30.69%), Space: 47.4 MB (40.59%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.js
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.js
@@ -1,35 +1,20 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
-var averageOfLevels = function(root) {
+const averageOfLevels = function(root) {
     if(!root)
         return [0]
     
-    let q = [root], res = []
+    let q = [root]
+    let res = []
     
     while(q.length>0){
-        let currLen = q.length;
-        if(currLen>0){
-            let val = 0;
-            for(let i=0;i<currLen;i++){
-                let curr = q.shift();
-                if(curr) {
-                    val += curr.val;
-                    if(curr.left) q.push(curr.left)
-                    if(curr.right) q.push(curr.right)
-                }
-            }
-            res.push(val/currLen)    
+        let currLen = q.length
+        let currVal = 0
+        for(let i=0;i<currLen;i++){
+            const curr = q.shift();
+            currVal += curr.val
+            if(curr.left) q.push(curr.left)
+            if(curr.right) q.push(curr.right)
         }
+        res.push(currVal/currLen)
     }
     return res
 };
