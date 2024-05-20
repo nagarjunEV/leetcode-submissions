1917781fd024911ecbb83eda9521cc43fc5f9db3
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2024-05-20 00:28:10 -0500

    Time: 77 ms (68.42%), Space: 62.9 MB (32.42%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,13 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    if(nums.length<2) return false
-    let map = {}
+    const obj = {}
     
     for(let i=0;i<nums.length;i++){
-        if(map[nums[i]])
+        if(obj[nums[i]])
             return true
-        map[nums[i]] = 1
+        obj[nums[i]] = 1
     }
     return false
 };
