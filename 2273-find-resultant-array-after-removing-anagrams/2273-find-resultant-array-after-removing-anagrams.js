5f7d748ca19613a6a7f796e1e27f27bca8e9bd9a
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2024-10-13 15:14:12 -0500

    Time: 73 ms (38.74%), Space: 55.4 MB (70.75%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.js
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.js
@@ -1,19 +1,37 @@
-const checkIfAnagrams = (w1, w2)=> {
-    if(w1.length != w2.length) return false
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+
+const areAnagrams = (str1, str2) => {
+   return str1.split("").sort().join("") === str2.split("").sort().join("");
+}
+
+const isAnagram = (str1, str2) => {
+    let anagramObj = {}
+    str1.split("").forEach(x => {
+        anagramObj[x] = anagramObj[x] ? ++anagramObj[x] : 1
+    })
     
-    w1 = w1.split('').sort().join('');
-    w2 = w2.split('').sort().join('')
-    return w1 == w2
+    str2.split("").forEach(y => {
+        anagramObj[y] = anagramObj[y] ? --anagramObj[y] : 1
+    })
+        
+    Object.keys(anagramObj).map(z => {
+        if(anagramObj[z] > 0)
+            return false;   
+    })
+    return true;
 }
 
 var removeAnagrams = function(words) {
-    
-    for(let i=1;i<words.length;){
-        let res = checkIfAnagrams(words[i-1], words[i])
-        if(res)
-            words.splice(i, 1)
-        else
-            i++
+    let i = 1;
+    while(i<words.length){
+            if(areAnagrams(words[i], words[i-1])){
+                words.splice(i, 1);
+            }else{
+                i++;
+        }
     }
-    return words
+    return words;
 };
