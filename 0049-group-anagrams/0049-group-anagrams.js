1407145842e1bb63a682228b326d4e3ba31bcbcd
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2024-10-13 15:35:57 -0500

    Time: 114 ms (63.94%), Space: 63.2 MB (48.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -1,35 +1,22 @@
-const isPrime = n => {
-    let i = 2;
-    while(i<=n/2){
-        if(n%i==0)
-            return false
-        i++
-    }
-    return true
-}
-
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
 var groupAnagrams = function(strs) {
-    let primeArr = [];
-    let n = 2;
-    while(primeArr.length<26){
-        if(isPrime(n))
-            primeArr.push(n)
-        n++;
-    }
+    const resObj = {}
+    const res = []
+    const customStrs = strs.map(str => str.split("").sort().join(""))
+
     
-    let map = {}, sumArr = []
-    for(let i=0;i<strs.length;i++){
-        let j=0, key = 1;
-        let str = strs[i]
-        while(j<str.length){
-            key *= primeArr[str[j].charCodeAt(0) - 97]
-            j++;
+    for(let i=0; i<customStrs.length; i++){
+        if(!resObj[customStrs[i]]){
+            resObj[customStrs[i]] = [strs[i]]
+        }else {
+            resObj[customStrs[i]].push(strs[i])
         }
-        if(map[key] != undefined)
-            map[key].push(strs[i])
-        else
-            map[key] = [strs[i]]
     }
-    
-    return Object.values(map);
+    Object.keys(resObj).map(key =>{
+      res.push(resObj[key])  
+    })
+   return res
 };
