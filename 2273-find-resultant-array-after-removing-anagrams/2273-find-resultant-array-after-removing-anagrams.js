3cf63846741c1dbfe077219054bb9d09659ff320
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2024-10-13 15:18:15 -0500

    Time: 104 ms (9.49%), Space: 61.1 MB (5.14%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.js
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.js
@@ -13,21 +13,27 @@ const isAnagram = (str1, str2) => {
         anagramObj[x] = anagramObj[x] ? ++anagramObj[x] : 1
     })
     
-    str2.split("").forEach(y => {
-        anagramObj[y] = anagramObj[y] ? --anagramObj[y] : 1
-    })
-        
+    for(let y of str2){
+        if(!anagramObj[y])
+            return false;
+        --anagramObj[y];
+    }
+    
     Object.keys(anagramObj).map(z => {
         if(anagramObj[z] > 0)
+            anagramObj = {}
             return false;   
     })
+    anagramObj = {}
     return true;
 }
 
 var removeAnagrams = function(words) {
     let i = 1;
     while(i<words.length){
-            if(areAnagrams(words[i], words[i-1])){
+            if(words[i].length != words[i-1].length)
+                i++;
+            else if(isAnagram(words[i], words[i-1])){
                 words.splice(i, 1);
             }else{
                 i++;
