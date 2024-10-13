d8b245e7e18bd7e8c50a796848ece5b06d5c87a1
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2024-10-13 15:46:41 -0500

    Time: 139 ms (14.89%), Space: 63.6 MB (38.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,20 +3,12 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    const resObj = {}
-    const res = []
-    const customStrs = strs.map(str => str.split("").sort().join(""))
-
-    
-    for(let i=0; i<customStrs.length; i++){
-        if(!resObj[customStrs[i]]){
-            resObj[customStrs[i]] = [strs[i]]
-        }else {
-            resObj[customStrs[i]].push(strs[i])
-        }
+    let obj = {}
+    for(let str of strs){
+        const s = str.split("").sort().join("");
+        if(!obj[s])
+            obj[s] = []
+        obj[s].push(str)
     }
-    Object.keys(resObj).map(key =>{
-      res.push(resObj[key])  
-    })
-   return res
+    return Object.values(obj);
 };
