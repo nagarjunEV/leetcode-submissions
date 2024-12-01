11a2afce799486f71f14dabf35b77b44ea020622
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2024-12-01 11:15:52 -0600

    Time: 73 ms (12.52%), Space: 50.2 MB (54.06%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -3,13 +3,11 @@
  * @return {Function}
  */
 var compose = function(functions) {
-	return function(x) {
-        let res;
-        for(let i = functions.length-1; i>=0;i--){
-            res = functions[i](x);
-            x = res;
+    return function(x) {
+        for(let i = functions.length -1; i >= 0 ; i--){
+            x = functions[i](x)
         }
-        return x;
+        return x
     }
 };
 
