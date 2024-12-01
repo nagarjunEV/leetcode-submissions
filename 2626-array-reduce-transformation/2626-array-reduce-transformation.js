fe0ce8ebe091de40a660584470235b706f474833
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2024-12-01 11:02:15 -0600

    Time: 56 ms (45.54%), Space: 50.5 MB (8.24%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -5,9 +5,9 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    
-    for(let i=0;i<nums.length;i++){
-        init = fn(init, nums[i]);
+    let curr = init;
+    for(let i=0; i<nums.length; i++){
+       init = fn(init, nums[i]);
     }
     return init;
 };
