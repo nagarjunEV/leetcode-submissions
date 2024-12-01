7c27074922845cb4a8aebf332a24f1b261b30f48
Nagarjun Enumudi Venkatesha <90735972+nagarjunEV@users.noreply.github.com>
2024-12-01 10:44:40 -0600

    Time: 50 ms (59.76%), Space: 49.1 MB (11.32%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -1,9 +1,10 @@
 /**
  * @return {Function}
  */
-var createHelloWorld = () => {
-    return () => "Hello World";
-        
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    }
 };
 
 /**
